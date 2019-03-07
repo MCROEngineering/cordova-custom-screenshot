fb75f1833c50f92cf8569cc5b43eeda2dd8a816d
Mihai Chifor <chifor.mihai21@gmail.com>
2019-03-07 12:54:09 +0200

    update Screenshot.js

--- www/Screenshot.js
+++ www/Screenshot.js
@@ -1,36 +1,77 @@
 /*
  *  MIT licensed
  */
+const cordova = require('cordova');
 const exec = require('cordova/exec');
 const ScreenShotFrame  = require('./ScreenShotFrame');
 const formats = ['png','jpg'];
 
-module.exports = {
-  getFrame: function() {
-    return ScreenShotFrame.apply(this, arguments);
-  },
+// module.exports = {
+//   getFrame: function() {
+//     return ScreenShotFrame.apply(this, arguments);
+//   },
+//
+//   save:function(callback, options) {
+//     var format = (options.format || 'png').toLowerCase();
+//     options.filename = options.filename || 'screenshot_'+Math.round((+(new Date()) + Math.random()));
+//     if(formats.indexOf(format) === -1){
+//       return callback && callback(new Error('invalid format '+format));
+//     }
+//     options.quality = typeof(options.quality) !== 'number'?100:options.quality;
+//     exec(function(res){
+//       callback && callback(null,res);
+//     }, function(error){
+//       callback && callback(error);
+//     }, "Screenshot", "saveScreenshot", [options]);
+//   },
+//
+//   URI:function(callback, options){
+//     options.quality = typeof(options.quality) !== 'number'?100:options.quality;
+//     exec(function(res){
+//       callback && callback(null, res);
+//     }, function(error){
+//       callback && callback(error);
+//     }, "Screenshot", "getScreenshotAsURI", [options]);
+//
+//   }
+// };
 
-  save:function(callback, options) {
-    var format = (options.format || 'png').toLowerCase();
-    options.filename = options.filename || 'screenshot_'+Math.round((+(new Date()) + Math.random()));
-    if(formats.indexOf(format) === -1){
-      return callback && callback(new Error('invalid format '+format));
-    }
-    options.quality = typeof(options.quality) !== 'number'?100:options.quality;
-    exec(function(res){
-      callback && callback(null,res);
-    }, function(error){
-      callback && callback(error);
-    }, "Screenshot", "saveScreenshot", [options]);
-  },
+// Empty constructor
+function Screenshot() {}
 
-  URI:function(callback, options){
-    options.quality = typeof(options.quality) !== 'number'?100:options.quality;
-    exec(function(res){
-      callback && callback(null, res);
-    }, function(error){
-      callback && callback(error);
-    }, "Screenshot", "getScreenshotAsURI", [options]);
-
-  }
+Screenshot.prototype.getFrame = function() {
+  return ScreenShotFrame.apply(this, arguments);
 };
+
+Screenshot.prototype.save = function(callback, options) {
+  var format = (options.format || 'png').toLowerCase();
+  options.filename = options.filename || 'screenshot_'+Math.round((+(new Date()) + Math.random()));
+  if(formats.indexOf(format) === -1){
+    return callback && callback(new Error('invalid format '+format));
+  }
+  options.quality = typeof(options.quality) !== 'number'?100:options.quality;
+  exec(function(res){
+    callback && callback(null,res);
+  }, function(error){
+    callback && callback(error);
+  }, "Screenshot", "saveScreenshot", [options]);
+};
+
+Screenshot.prototype.URI = function(callback, options){
+  options.quality = typeof(options.quality) !== 'number'?100:options.quality;
+  exec(function(res){
+    callback && callback(null, res);
+  }, function(error){
+    callback && callback(error);
+  }, "Screenshot", "getScreenshotAsURI", [options]);
+
+};
+
+Screenshot.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.toastyPlugin = new Screenshot();
+  return window.plugins.toastyPlugin;
+};
+cordova.addConstructor(Screenshot.install);
