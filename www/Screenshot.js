73d81d33d2c3722c087971de74634e3a089963cc
Mihai Chifor <chifor.mihai21@gmail.com>
2019-03-13 12:54:44 +0200

    [plugin]: rename and update plugin functions

--- www/Screenshot.js
+++ www/Screenshot.js
@@ -6,34 +6,30 @@ const ScreenShotFrame = require('./ScreenShotFrame');
 const formats = ['png', 'jpg'];
 
 module.exports = {
-  getFrame: function (options, callback) {
-    var that = this;
-    return ScreenShotFrame.getFrame(options, function(updatedOptions) {
-      console.log(updatedOptions);
-      that.save(callback, updatedOptions);
+  save: function (options, callback) {
+    return ScreenShotFrame.getFrame(options, (updatedOptions) => {
+      const format = (options.format || 'png').toLowerCase();
+      updatedOptions.filename = updatedOptions.filename || 'screenshot_' + Math.round((+(new Date()) + Math.random()));
+      if (formats.indexOf(format) === -1) {
+        return callback && callback(new Error('invalid format ' + format));
+      }
+      updatedOptions.quality = typeof (updatedOptions.quality) !== 'number' ? 100 : updatedOptions.quality;
+      exec(function (res) {
+        callback && callback(null, res);
+      }, function (error) {
+        callback && callback(error);
+      }, 'Screenshot', 'saveScreenshot', [updatedOptions]);
     }).showScreenShotModal();
   },
 
-  save: function (callback, options) {
-    const format = (options.format || 'png').toLowerCase();
-    options.filename = options.filename || 'screenshot_' + Math.round((+(new Date()) + Math.random()));
-    if (formats.indexOf(format) === -1) {
-      return callback && callback(new Error('invalid format ' + format));
-    }
-    options.quality = typeof (options.quality) !== 'number' ? 100 : options.quality;
-    exec(function (res) {
-      callback && callback(null, res);
-    }, function (error) {
-      callback && callback(error);
-    }, 'Screenshot', 'saveScreenshot', [options]);
-  },
-
-  URI: function (callback, options) {
-    options.quality = typeof (options.quality) !== 'number' ? 100 : options.quality;
-    exec(function (res) {
-      callback && callback(null, res);
-    }, function (error) {
-      callback && callback(error);
-    }, 'Screenshot', 'getScreenshotAsURI', [options]);
+  getBase64: function (options, callback) {
+    return ScreenShotFrame.getFrame(options, (updatedOptions) => {
+      updatedOptions.quality = typeof (updatedOptions.quality) !== 'number' ? 100 : updatedOptions.quality;
+      exec(function (res) {
+        callback && callback(null, res);
+      }, function (error) {
+        callback && callback(error);
+      }, 'Screenshot', 'getScreenshotAsURI', [updatedOptions]);
+    }).showScreenShotModal();
   },
 };
